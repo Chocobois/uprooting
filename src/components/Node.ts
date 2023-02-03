31b717aa36b3cec515d94513b838f8a023b53649
Golen <golen87@hotmail.com>
2023-02-03 23:24:48 +0100

    Add generous node input hitarea. Disable nodes with 2 or more children.

--- src/components/Node.ts
+++ src/components/Node.ts
@@ -5,6 +5,7 @@ export class Node extends Button {
 	private image: Phaser.GameObjects.Image;
 
 	public parent: Node | null;
+	public children: Node[];
 	public score: number;
 
 	constructor(scene: BaseScene, x: number, y: number, root: boolean=false) {
@@ -12,20 +13,23 @@ export class Node extends Button {
 		this.scene.add.existing(this);
 
 		this.parent = null;
+		this.children = [];
 		this.score = 1;
 
 
 		this.image = this.scene.add.image(x, y, "circle");
-		this.image.setScale((root ? 40 : 10) / this.image.width);
+		this.image.setScale((root ? 25 : 15) / this.image.width);
 
 		this.bindInteractive(this.image, true);
-		// const inputPadding = 1000;
-		// this.image.input.hitArea.setTo(-inputPadding, -inputPadding, this.image.width+2*inputPadding, this.image.height+2*inputPadding);
+		const inputPadding = 40 / this.image.scaleX;
+		this.image.input.hitArea.setTo(-inputPadding, -inputPadding, this.image.width+2*inputPadding, this.image.height+2*inputPadding);
 		// this.scene.input.enableDebug(this.image);
 	}
 
 	update(time: number, delta: number) {
-		this.image.setTint(this.hold ? 0xFF0000 : this.hover ? 0xFFFF00 : 0xFFFFFF);
+		if (this.image.input.enabled) {
+			this.image.setTint(this.hold ? 0xFF0000 : this.hover ? 0xFF7700 : 0xFFFFFF);
+		}
 	}
 
 
@@ -35,6 +39,18 @@ export class Node extends Button {
 
 	addParent(node: Node) {
 		this.parent = node;
+		this.parent.addChild(node);
+	}
+
+	addChild(node: Node) {
+		this.children.push(node);
+
+		// If a node has more than 2 children, disable node interaction
+		if (this.children.length >= 2) {
+			this.image.input.enabled = false;
+			this.image.setTint(0xFFFFFF);
+			this.image.setAlpha(0.3);
+		}
 	}
 
 	addScore() {
