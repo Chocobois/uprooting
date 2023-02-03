d76bf35cfadc15d0b52d069d1aba401775c4966c
Dan-Eric Næss <dan-eric.naess@live.no>
2023-02-03 15:15:37 +0100

    Update build config, add desktop app

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
 import { defineConfig } from 'vite'
+import zip from 'vite-plugin-zip-pack';
+import { execSync } from 'child_process';
 
 // https://vitejs.dev/config
 export default defineConfig({
   root: 'src',
-  plugins: [],
+  plugins: [
+    {
+      name: 'neu-build',
+      apply: 'build',
+      closeBundle() {
+        console.log('Building standalone app')
+        execSync('neu build');
+      }
+    },
+    zip({
+      inDir: './dist/unpacked',
+      outDir: './dist',
+      outFileName: 'game-web.zip'
+    })
+  ],
   build: {
-    outDir: '../dist'
+    outDir: '../dist/unpacked',
+    chunkSizeWarningLimit: 4096
   }
 });
