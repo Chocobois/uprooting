16ab4e2ab5d41b5c999eaf2447efd9b405c5e44d
Lumie <lumie.kyo@gmail.com>
2023-02-04 16:56:10 -0500

    rock

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -53,6 +53,7 @@ import sapphire from "./images/items/sapphire.png";
 import emerald from "./images/items/emerald.png";
 import diamond from "./images/items/diamond.png";
 import platinum from "./images/items/platinum.png";
+import badrock from "./images/items/hasty_rock.png";
 
 // UI
 import fruit_upgrade from "./images/ui/fruit_upgrade.png";
@@ -100,6 +101,7 @@ const images: Asset[] = [
 	{ key: "emerald", path: emerald },
 	{ key: "diamond", path: diamond },
 	{ key: "platinum", path: platinum },
+	{ key: "badrock", path: badrock },
 
 	// UI
 	{ key: "fruit_upgrade", path: fruit_upgrade },
