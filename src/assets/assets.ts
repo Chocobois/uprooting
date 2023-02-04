072ec4b24f1738fe6dc262305dcc1130cbbe13b7
Golen <golen87@hotmail.com>
2023-02-04 12:19:14 +0100

    Organize assets. Add new images to preload.

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,4 +1,4 @@
-/* Interface */
+/* Interfaces */
 
 interface Asset {
 	key: string;
@@ -19,38 +19,84 @@ interface Audio {
 	rate?: number;
 }
 
-import circle from "../assets/images/circle.png";
-import overworld from "../assets/images/overworld.png";
-import underground from "../assets/images/underground.png";
-import tree from "../assets/images/tree.png";
-import tree_little from "../assets/images/tree_little.png";
-import sapling from "../assets/images/sapling.png";
-import green_magic from "../assets/images/greencurvy.png";
 
+/* Images */
+
+// Backgrounds
+import overworld from "../assets/images/backgrounds/overworld.png";
+import underground from "../assets/images/backgrounds/underground.png";
+
+// Tree
+import tree from "../assets/images/tree/tree.png";
+import tree_little from "../assets/images/tree/tree_little.png";
+import sapling from "../assets/images/tree/sapling.png";
+import rootline from "../assets/images/tree/rootline.png";
+
+// Items
+import apple from "../assets/images/items/apple.png";
+import bone from "../assets/images/items/bone.png";
+import dragonfruit from "../assets/images/items/dragonfruit.png";
+
+// UI
+import fruit_upgrade from "../assets/images/ui/fruit_upgrade.png";
+import energy_teter from "../assets/images/ui/meter.png";
+import prompt from "../assets/images/ui/prompt.png";
+
+// Effects
+import green_magic from "../assets/images/effects/green_magic.png";
+
+// Other
+import circle from "../assets/images/circle.png";
 
 const images: Asset[] = [
-	{ key: "circle",	path: circle },
-	{ key: "overworld",	path: overworld },
-	{ key: "underground",	path: underground },
-	{ key: "tree",	path: tree },
-	{ key: "tree_little",	path: tree_little },
-	{ key: "sapling",	path: sapling },
+	// Backgrounds
+	{ key: "overworld", path: overworld },
+	{ key: "underground", path: underground },
+
+	// Tree
+	{ key: "tree", path: tree },
+	{ key: "tree_little", path: tree_little },
+	{ key: "sapling", path: sapling },
+	{ key: "rootline", path: rootline },
+
+	// Items
+	{ key: "apple", path: apple },
+	{ key: "bone", path: bone },
+	{ key: "dragonfruit", path: dragonfruit },
+
+	// UI
+	{ key: "fruit_upgrade", path: fruit_upgrade },
+	{ key: "energy_teter", path: energy_teter },
+	{ key: "prompt", path: prompt },
+
+	// Effects
+	{ key: "green_magic", path: green_magic },
+
+	// Other
+	{ key: "circle", path: circle },
 ];
+
+
+
+/* Spritesheets */
 
 const spritesheets: SpriteSheet[] = [
 	{ key: "green_magic", path: green_magic, width: 128, height: 128 },
 ];
 
 
-/* UI */
+
+/* Audio */
+
+// UI
 import m_main_menu from "../assets/music/title.mp3";
 
-/* Music */
+// Music
 import m_first from "../assets/music/first.mp3";
 import m_first_draw from "../assets/music/first_draw.mp3";
 import m_first_end from "../assets/music/first_end.mp3";
 
-/* SFX: Roots */
+// SFX: Roots
 import r_grow from "../assets/sounds/roots/grow.mp3";
 import r_place from "../assets/sounds/roots/place.mp3";
 import r_place_error from "../assets/sounds/roots/place_error.mp3";
@@ -64,6 +110,8 @@ const audios: Audio[] = [
   	{ key: "r_place", path: r_place, volume: 1 },
 	{ key: "r_place_error", path: r_place_error, volume: 0.5 },
 ];
+
+
 
 export {
 	images,
