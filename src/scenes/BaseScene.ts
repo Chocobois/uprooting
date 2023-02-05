9586447db54b759a87d62a1f64f694e9ae53a71b
Dan-Eric Næss <dan-eric.naess@live.no>
2023-02-05 12:20:04 +0100

    Permanent marker font

--- src/scenes/BaseScene.ts
+++ src/scenes/BaseScene.ts
@@ -54,10 +54,9 @@ export class BaseScene extends Phaser.Scene {
 	// Creates Phaser text object
 	createText(x: number=0, y: number=0, size: number=20, color: string="#FFF", text: string=""): Phaser.GameObjects.Text {
 		return this.add.text(x, y, text, {
-			fontFamily: "VCR_OSD_MONO",
+			fontFamily: "PermanentMarker-Regular",
 			fontSize: Math.max(size*10, 1) + "px",
-			color: color,
-			fontStyle: "bold"
+			color: color
 		}).setLineSpacing(0.4*size).setPadding(2).setScale(0.1);
 	}
 
